25423b98fdb99335738b0032eddcb6dd83bbc3a6
David <davidmeza@outlook.com>
2015-09-30 17:04:14 -0400

    player v. player game fully functional

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,8 @@ require 'colorize'
 
 # Handles all player-related functionality
 class Player
-  attr_accessor :name
-  attr_reader :piece
+
+  attr_reader :piece, :name
 
   def initialize(name = "Mystery Player", piece, board)
     @name = name
@@ -37,12 +37,13 @@ class Human < Player
 
   def validate_coordinates_format(coords)
     valid = coords.is_a?(Array) && coords.size == 2
-    puts "Your coordinates are not in the right format. Try x,y (e.g '1,1')" unless valid
+    puts "Your coordinates are not in the right format. Try x,y (e.g '1,1')".colorize(:yellow) unless valid
     valid
   end
 
   def ask_for_coordinates
-    puts "#{@name}(#{@piece}), enter your coordinates in the form x,y:".colorize(:blue)
+    print "#{@name}(#{@piece}), enter your coordinates in the form x,y: ".colorize(:blue)
+    # In Ruby strings to_i return 0 so we don't need to worry about validating whether it's an int (it won't explode in our faces)
     gets.strip.split(",").map(&:to_i)
   end
 
@@ -51,8 +52,6 @@ end
 # Manages board-related functionality such as rendering and checking for victory
 class Board
 
-  attr_reader :board_arr
-
   # Initialize the board as blank unless we are passed one (can expand to saving a game)
   def initialize(board_arr = nil)
     # I'll use a 2D array instead of one giant array
@@ -60,15 +59,17 @@ class Board
   end
 
   def add_piece(coords, piece)
-    # On the board, the axes are actually reversed
-    @board_arr[coords[1]][coords[0]] = piece
+    if location_valid?(coords)
+      # On the board, the axes are actually reversed (rows are x and cols are y)
+      @board_arr[coords[1]][coords[0]] = piece
+    end
   end
 
   def render
     puts
-    puts "x       0   1   2"
-    puts "------------------"
-    puts "y |"
+    puts "x".colorize(:magenta) + "       0   1   2"
+    puts "------------------".colorize(:light_black)
+    puts "y".colorize(:magenta) + " |".colorize(:light_black)
     @board_arr.each_with_index do |row, row_index|
       print "#{row_index} |    "
       row.each_with_index do |cell, cell_index|
@@ -82,6 +83,7 @@ class Board
   end
 
   def winning_combination?(piece)
+    # Can this piece win the game in any of these combinations?
     winning_diagonal(piece) || winning_horizontal(piece) || winning_vertical(piece)
   end
 
@@ -91,6 +93,24 @@ class Board
   end
 
     private
+
+    def location_valid?(coords)
+      if inside_board?(coords)
+        coordinates_available?(coords)
+      end
+    end
+
+    def inside_board?(coords)
+      inside = coords.all? { |value| (0..2).include?(value)  }
+      puts "Coordinates are out of bounds. Try something between 0,0 and 2,2".yellow unless inside
+      inside
+    end
+
+    def coordinates_available?(coords)
+      available = @board_arr[coords[1]][coords[0]].nil?
+      puts "There is already a piece in that cell. Try a different one".yellow unless available
+      available
+    end
 
     def winning_diagonal(piece)
       diagonals.any? { |diag| diag.all? { |cell| cell == piece  }  }
@@ -105,13 +125,11 @@ class Board
     end
 
     def horizontals
-      # Store the result in an instance variable so we only generate it once
-      return @h if @h
       hor_arr = [];
       3.times do |i|
         hor_arr << [ @board_arr[i][0], @board_arr[i][1], @board_arr[i][2] ]
       end
-      @h = hor_arr
+      hor_arr
     end
 
     def winning_vertical(piece)
@@ -119,12 +137,11 @@ class Board
     end
 
     def verticals
-      return @v if @v
       vert_arr = [];
       3.times do |i|
         vert_arr << [ @board_arr[0][i], @board_arr[1][i], @board_arr[2][i] ]
       end
-      @v = vert_arr
+      vert_arr
     end
 
 end
@@ -145,7 +162,7 @@ class Game
     print_game_start
 
     loop do
-      puts "It is now #{@current_player.name}'s turn."
+      puts "It is now #{@current_player.name}'s turn.".colorize(:cyan)
       @board.render
       @current_player.get_coordinates
       break if game_is_over
@@ -159,10 +176,18 @@ class Game
     def print_welcome
       puts "Welcome to my Tic Tac Toe game"
       puts "Let's start by setting up our game"
-      print "First, select the marker player #1 will use (X or O or any other symbol): "
+      print "First, select the marker player #1 will use " + "(X or O or any other symbol):".light_blue.on_light_white + " "
       @piece1 = gets.strip[0].to_sym
       print "Great, how about the marker for player #2? "
       @piece2 = gets.strip[0].to_sym
+      ask_for_piece_again if @piece1 == @piece2
+    end
+
+    def ask_for_piece_again
+      puts "Both pieces can't be the same! What's the fun in that".yellow
+      print "Please enter a marker for player #2 again "
+      @piece2 = gets.strip[0].to_sym
+      ask_for_piece_again if @piece1 == @piece2
     end
 
     def print_game_type
@@ -178,14 +203,14 @@ class Game
 
       case input
       when "1" || "(1)" || "1."
-        print "What's your name? "
+        print "What's your name?".red.on_light_white + " "
         p1_name = gets.chomp
-        print "And player #2's name? "
+        print "And player #2's name?".red.on_light_white + " "
         p2_name = gets.chomp
         @player1 = Human.new(p1_name, @piece1, @board)
         @player2 = Human.new(p2_name, @piece2, @board)
       when "2" || "(2)" || "2."
-        print "What's your name? "
+        print "What's your name?".red.on_light_white + " "
         p1_name = gets.chomp
         @player1 = Human.new(p1_name, @piece1, @board)
         @player2 = Computer.new(nil, @piece2, @board)
@@ -193,14 +218,14 @@ class Game
         @player1 = Computer.new(nil, @piece1, @board)
         @player2 = Computer.new(nil, @piece2, @board)
       else
-        puts "Sorry, I didn't understand that. Try selecting 1, 2, or 3"
+        puts "Sorry, I didn't understand that. Try selecting 1, 2, or 3".colorize(:yellow)
         # Make recursive call until we get a good input
         select_players
       end
     end
 
     def print_selected_players
-      puts "Great. So this will be a game of #{@player1.class.to_s} v. #{@player2.class.to_s}"
+      puts "Great. So this will be a game of " + "#{@player1.class.to_s} v. #{@player2.class.to_s}".light_blue.on_light_white
       print "Now who will move first (select 1 or 2)? "
     end
 
@@ -212,7 +237,7 @@ class Game
       when "2" || "(2)" || "2."
         @current_player = @player2
       else
-        puts "Sorry, I didn't understand that. Try selecting 1, 2"
+        puts "Sorry, I didn't understand that. Try selecting 1, 2".colorize(:yellow)
         # Make recursive call until we get a good input
         select_first_player
       end
@@ -224,18 +249,18 @@ class Game
     end
 
     def game_is_over
-      p (check_victory || check_draw)
+      check_victory || check_draw
     end
 
     def check_victory
       win = @board.winning_combination?(@current_player.piece)
-      puts "Congratulations #{@current_player.name}, you win!" if win
+      puts "Congratulations #{@current_player.name}, you win!".colorize(:green) if win
       win
     end
 
     def check_draw
       draw = @board.full?
-      puts "Bummer, you've drawn..." if draw
+      puts "Bummer, you've drawn...".colorize(:light_blue).on_blue.underline if draw
       draw
     end
 
@@ -245,107 +270,6 @@ class Game
 
 end
 
-
-# class Game
-#   def initialize
-#     @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-#     @com = "X"
-#     @hum = "O"
-#   end
-
-#   def start_game
-#     puts "Welcome to my Tic Tac Toe game"
-#     puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|\n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|\n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|\n"
-#     puts "Please select your spot."
-#     until game_is_over(@board) || tie(@board)
-#       get_human_spot
-#       if !game_is_over(@board) && !tie(@board)
-#         eval_board
-#       end
-#       puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|\n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|\n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|\n"
-#     end
-#     puts "Game over"
-#   end
-
-#   def get_human_spot
-#     spot = nil
-#     until spot
-#       spot = gets.chomp.to_i
-#       if @board[spot] != "X" && @board[spot] != "O"
-#         @board[spot] = @hum
-#       else
-#         spot = nil
-#       end
-#     end
-#   end
-
-#   def eval_board
-#     spot = nil
-#     until spot
-#       if @board[4] == "4"
-#         spot = 4
-#         @board[spot] = @com
-#       else
-#         spot = get_best_move(@board, @com)
-#         if @board[spot] != "X" && @board[spot] != "O"
-#           @board[spot] = @com
-#         else
-#           spot = nil
-#         end
-#       end
-#     end
-#   end
-
-#   def get_best_move(board, next_player, depth = 0, best_score = {})
-#     available_spaces = []
-#     best_move = nil
-#     board.each do |s|
-#       if s != "X" && s != "O"
-#         available_spaces << s
-#       end
-#     end
-#     available_spaces.each do |as|
-#       board[as.to_i] = @com
-#       if game_is_over(board)
-#         best_move = as.to_i
-#         board[as.to_i] = as
-#         return best_move
-#       else
-#         board[as.to_i] = @hum
-#         if game_is_over(board)
-#           best_move = as.to_i
-#           board[as.to_i] = as
-#           return best_move
-#         else
-#           board[as.to_i] = as
-#         end
-#       end
-#     end
-#     if best_move
-#       return best_move
-#     else
-#       n = rand(0..available_spaces.count)
-#       return available_spaces[n].to_i
-#     end
-#   end
-
-#   def game_is_over(b)
-
-#     [b[0], b[1], b[2]].uniq.length == 1 ||
-#     [b[3], b[4], b[5]].uniq.length == 1 ||
-#     [b[6], b[7], b[8]].uniq.length == 1 ||
-#     [b[0], b[3], b[6]].uniq.length == 1 ||
-#     [b[1], b[4], b[7]].uniq.length == 1 ||
-#     [b[2], b[5], b[8]].uniq.length == 1 ||
-#     [b[0], b[4], b[8]].uniq.length == 1 ||
-#     [b[2], b[4], b[6]].uniq.length == 1
-#   end
-
-#   def tie(b)
-#     b.all? { |s| s == "X" || s == "O" }
-#   end
-
-# end
 
 game = Game.new
 game.start_game
